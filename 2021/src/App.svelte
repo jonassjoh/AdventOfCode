3f7ead3be8c160e966f4b7116a15f50d69e3d005
jonassjoh <jonassjoh@gmail.com>
2021-12-05 19:08:16 +0100

    2021 1

---
+++ 2021/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Day1 from "./days/1/Day1.svelte";
+</script>
+
+<main>
+  <h1>Advent of Code 2021!</h1>
+
+  <Day1 />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
---
+++ 2021/src/days/1/Day1.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { prevent_default } from "svelte/internal";
+
+  import { _sample, _input } from "./input.js";
+
+  const input = _input.split("\n").map(Number);
+
+  const part1 = (res) => {
+    return res.reduce((acc, _, i, arr) => acc + (arr[i - 1] < arr[i]), 0);
+  };
+
+  const part2 = (edgeSize = 1) => {
+    const res = input.map((_, i, arr) => {
+      return arr
+        .slice(i - edgeSize, i + edgeSize + 1)
+        .reduce((acc, _, __, arr) => arr, undefined)
+        ?.reduce((acc, v) => acc + v);
+    }, 0);
+    return part1(res);
+  };
+</script>
+
+<div class="vertical">
+  <code>{part1(input)}</code>
+  <code>{part2()}</code>
+</div>
